453f814331b6d35b9e8819c2870746173c0be54d
ktmitton <kdrive113@gmail.com>
2022-07-16 02:14:34 -0400

    Added debug output

--- Tasks/index.ts
+++ Tasks/index.ts
@@ -1,4 +1,4 @@
-import { getInput, setResult, TaskResult, getEndpointAuthorizationParameterRequired, getEndpointDataParameterRequired, getEndpointUrlRequired } from 'azure-pipelines-task-lib';
+import { getInput, setVariable, setResult, TaskResult, getEndpointAuthorizationParameterRequired, getEndpointDataParameterRequired, getEndpointUrlRequired } from 'azure-pipelines-task-lib';
 import fetch from 'node-fetch';
 
 async function run() {
@@ -12,17 +12,21 @@ async function run() {
     const vaultRole = getEndpointDataParameterRequired(hashicorpId, 'vaultRole');
     const consulRole = getEndpointDataParameterRequired(hashicorpId, 'consulRole');
 
+    console.log('roleId', roleId);
+    console.log('secretId', secretId);
     const login = await fetch(`${serverUrl}/v1/auth/approle/login`, {
       method: 'POST',
       body: JSON.stringify({ 'role_id': roleId, 'secret_id': secretId })
     });
+    console.log('login', login);
     const loginDetails = await login.json() as any;
-    const token = loginDetails.auth.client_token;
+    console.log('loginDetails', loginDetails);
+    const loginToken = loginDetails.auth.client_token;
 
     const vault = await fetch(`${serverUrl}/v1/auth/token/create/${vaultRole}`, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${token}`
+        'Authorization': `Bearer ${loginToken}`
       },
       body: JSON.stringify({})
     });
@@ -32,16 +36,19 @@ async function run() {
     const consul = await fetch(`${serverUrl}/v1/consul/creds/${consulRole}`, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${token}`
+        'Authorization': `Bearer ${loginToken}`
       },
       body: JSON.stringify({})
     });
     const consulDetails = await consul.json() as any;
     const consulToken = vaultDetails.auth.client_token;
 
-    console.log('login', loginDetails);
     console.log('vault', vaultDetails);
     console.log('consul', consulDetails);
+
+    setVariable("loginToken", loginToken, false, false);
+    setVariable("vaultToken", vaultToken, false, false);
+    setVariable("consulToken", consulToken, false, false);
   }
   catch (err: any) {
     setResult(TaskResult.Failed, err.message);
